1a46cc3fdcc475e0413b1bf7373651ab8dd96d25
Akim <akimik2002@gmail.com>
2024-05-07 17:50:05 +0300

    первая страница

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".background-slider", {
     effect: "fade",
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
     autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+const blogSwiper = new Swiper(".blog__slider", {
+  loop: true,
+  slidesPerView: 1,
+  // spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      // spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      // spaceBetween: 70,
+    },
+  },
+  autoplay: {
     delay: 3000,
     disableOnInteraction: false,
   },
